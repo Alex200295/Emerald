8ab24b5ab04b8574cd8e3985a7a0b8cdec0127a5
Alexis <alex200295@hotmail.fr>
2025-11-16 13:32:28 +0100

    swapping files and folder for git

---
+++ Emerald/Assets/Project/Scripts/Player/Movement/PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+/// <summary>
+/// Contrôleur principal du joueur.
+/// Gère la capture des entrées utilisateurs et orchestre les composants de mouvements et de caméra
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+
+    
+}
